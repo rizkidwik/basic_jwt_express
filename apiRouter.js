af452542677b1dceca67b51c1ff71720c253f73c
rizkidwik <rizkidwi123@gmail.com>
2022-08-21 21:27:54 +0700

    login & register api

--- apiRouter.js
+++ apiRouter.js
@@ -3,33 +3,60 @@
 const express = require("express");
 const apiRouter = express.Router();
 const db = require("./db.js");
-
-apiRouter.get("/", async (req, res) => {
+const { hashSync, genSaltSync, compareSync } = require("bcrypt");
+const jsonwebtoken = require('jsonwebtoken');
+apiRouter.post('/register', async (req,res,next)=>{
     try {
-        const mahasiswa = await db.getAllMahasiswa();
-        res.status(200).json({mahasiswa: mahasiswa});
+        const username = req.body.username;
+        const email = req.body.email;
+        let password = req.body.password
+            if(!username || !email || !password ) {
+                return res.sendStatus(400);
+            }
+            
+            const salt = genSaltSync(10);
+            password = hashSync(password,salt);
+
+            const user = await db.insertUser(username,email,password);
+
+            const jsontoken = jsonwebtoken.sign({user:user},process.env.SECRET_KEY,{expiresIn:'30m'});
+            res.cookie('token', jsontoken, { httpOnly: true,  SameSite: 'strict' , 
+            expires: new Date(Number(new Date()) + 30*60*1000) }); //we add secure: true, when using https.
+            res.json({token:jsontoken});
+        
     } catch(err) {
         console.log(err)
-        res.sendStatus(500);
+        res.sendStatus(400);
     }
 })
 
-apiRouter.get('/:mahasiswaId',(req,res,next)=>{
-    res.status(200).json({mahasiswa: req.mahasiswa});
-    
-})
-
-apiRouter.post('/', async (req,res,next)=>{
+apiRouter.post('/login',async(req,res,next)=>{
     try {
-        const name = req.body.mahasiswa.name;
-        const npm = req.body.mahasiswa.npm;
-            if(!name || !npm ) {
-                return res.sendStatus(400);
+        const email = req.body.email;
+        const password = req.body.password;
+        user = await db.getUserByEmail(email);
+        
+            if(!user ) {
+                return res.json({
+                    message: "Invalid email or password"
+                })
             }
-        const mahasiswa = await db.insertMahasiswa(name,npm).then(insertId=>{return db.getOneEmployee(insertId)});
+            
+            const isValidPassword = compareSync(password,user[0].password);
+            
+            if(isValidPassword){
+                // user.password = undefined;
+                const jsontoken = jsonwebtoken.sign({user:user},process.env.SECRET_KEY,{expiresIn:'30m'});
+                res.cookie('token', jsontoken, { httpOnly: true,  SameSite: 'strict' , 
+                expires: new Date(Number(new Date()) + 30*60*1000) }); //we add secure: true, when using https.
+                res.json({token:jsontoken});
+            } else {
+                return res.json({
+                    message: "Invalid email or password"
+                });
+            }
     } catch(err) {
         console.log(err)
-        res.sendStatus(500);
     }
 })
 
